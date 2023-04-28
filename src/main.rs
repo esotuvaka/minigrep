1cfdb159b5f630163eb700dfdbc0fedf1beb0503
PiqqiDesigns <eric.otuvaka@gmail.com>
2023-04-28 13:12:09 -0700

    fix: slight edit for exe

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env; 
 use std::process; 
 
-use minigrep::Config;
+extern crate minigrep;
+
+use minigrep::Config; 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,7 +12,7 @@ fn main() {
         eprintln!("Problem parsing arguments: {}", err); 
         process::exit(1);
     });
-
+ 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename); 
 
